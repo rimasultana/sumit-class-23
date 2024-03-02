c015124731dedb5f9aee8b302e93542a3a878682
rimasultana <rimasultana20225@gmail.com>
2024-03-02 19:07:22 +0600

    searech

---
+++ index.js
@@ -0,0 +1,5 @@
+const fruits = ["Banana","Orange","Apple","Labu","zero"];
+document.getElementById("demo1").innerHTML = fruits;
+
+fruits.sort();
+document.getElementById("demo2").innerHTML = fruits;
