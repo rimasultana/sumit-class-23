fc6bb3e5466242f653905e4a7c864b68aaba7ff6
rimasultana <rimasultana20225@gmail.com>
2024-03-02 19:13:51 +0600

    sort

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ document.getElementById("demo1").innerHTML = fruits;
 
 fruits.sort();
 document.getElementById("demo2").innerHTML = fruits;
+
+fruits.reverse();
+document.getElementById("demo3").innerHTML = fruits;
